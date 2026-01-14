b4f7d3a3ebb09c790f897730208f0df33cf77f95
Bububooo3 <Bububooo3@gmail.com>
2026-01-14 17:43:10 -0500

    edit

--- robot-code/src/main/java/frc/robot/Superstructure.java
+++ robot-code/src/main/java/frc/robot/Superstructure.java
@@ -66,12 +66,6 @@ package frc.robot;
  *
  * ========================================================================
  */
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.swerve.SwerveDrive;
 
 /**
