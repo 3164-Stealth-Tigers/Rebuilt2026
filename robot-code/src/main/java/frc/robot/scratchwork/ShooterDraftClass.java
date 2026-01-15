2389441b29682887f0ea0a3640531e867cc0c4af
Rhys-Marriott <156854616+Rhys-Marriott@users.noreply.github.com>
2026-01-15 16:27:11 -0500

    add coments

--- robot-code/src/main/java/frc/robot/scratchwork/ShooterDraftClass.java
+++ robot-code/src/main/java/frc/robot/scratchwork/ShooterDraftClass.java
@@ -3,63 +3,54 @@ package frc.robot.scratchwork;
 import frc.robot.scratchwork.VisionDraftClass;
 
 public class ShooterDraftClass {
-  static double visionx() {
-    return 2;
-  }
-  static double visiony() {
-    return 4.034663;
-  }
-  static double visionz() {
-    return 0;
-  }
-  static double visionAngle() {
-    return 0;
-  }
   static double hubAngle() {
     double x, y, hubx, huby;
-    x = visionx();
-    y = visiony();
-    huby = 4.034663;
-    if (x < 4.625594) {
+    x = visionx(); // find x coordinate of robot
+    y = visiony(); // find y coordinate of robot
+    huby = 4.034663; // set universal y coordinate of hub
+    if (x <= 4.625594) {
       hubx = 4.625594;
-    }
-    else if (x > 11.915394) {
+    } // determine if robot is in red alliance zone and set x coordinate of hub accordingly 
+    else if (x >= 11.915394) {
       hubx = 11.915394;
-    }
+    } // determine if robot is in blue alliance zone and set x coordinate of hub accordingly 
     else {
-      return 0;
-    }
-    return Math.atan((y - huby) / (x - hubx));
-  }
+      return -180;
+    } // determine if robot is in neutral zone and return invalid value, showing not to run
+    return visionAngle() - Math.atan((huby - y) / (hubx - x)) * 57.2957795131; // calculate hub angle, convert it to degrees, and subtract it from robot angle
+  } // determine angle difference between robot angle and hub, in degrees
+  
   static double hubDistance() {
     double x, y, hubx, huby;
-    x = visionx();
-    y = visiony();
-    huby = 4.034663;
-    if (x < 4.625594) {
+    x = visionx(); // find x coordinate of robot
+    y = visiony(); // find y coordinate of robot
+    huby = 4.034663; // set universal y coordinate of hub
+    if (x <= 4.625594) {
       hubx = 4.625594;
-    }
-    else if (x > 11.915394) {
+    } // determine if robot is in red alliance zone and set x coordinate of hub accordingly 
+    else if (x >= 11.915394) {
       hubx = 11.915394;
-    }
+    } // determine if robot is in blue alliance zone and set x coordinate of hub accordingly 
     else {
       return 0;
-    }
-    return Math.sqrt(Math.pow(hubx - x, 2) + Math.pow(huby - y, 2));
-  }
+    } // determine if robot is in neutral zone and return invalid value, showing not to run
+    return Math.sqrt(Math.pow(hubx - x, 2) + Math.pow(huby - y, 2)); // calculate distance between robot and hub using pythagorean theorem
+  } // determine distance between robot and hub, in meters
+  
   static double shootVelocity() {
-    double deltax = hubDistance();
+    double deltax = hubDistance(); // find distance between robot and hub, in meters
     if (deltax == 0) {
       return 0;
-    }
-    double theta = 0.785398;
-    double g = 9.8067;
-    double yInit = visionz() + 0.5;
-    double yFinal = 1.8288;
-    return deltax / Math.cos(theta) * Math.sqrt(g / 2 * (yInit - yFinal + deltax * Math.tan(theta)));
-  }
+    } // determine if robot is in neutral zone and return zero if so
+    double theta = 45; // launch angle of fuel, in degrees
+    double g = 9.8067; // acceleration due to gravity in meters per second squared
+    double yInit = visionz() + 0.5; // height of fuel at launch
+    double yFinal = 1.8288; // height of the hub rim
+    return deltax / Math.cos(theta * 0.01745329251) * Math.sqrt(g / 2 * (yInit - yFinal + deltax * Math.tan(theta * 0.01745329251))); // calculate velocity needed using kinematics
+  } // determine velocity needed to shoot the ball into the hub, in meters per second squared
+  
   public static void main(String args[]) {
     System.out.println("Velocity Needed: " + shootVelocity() + " meters per second.");
-    System.out.println("Angle Change Needed: " + hubAngle() + " radians.");
+    System.out.println("Angle Change Needed: " + hubAngle() + " degrees.");
   }
 }
