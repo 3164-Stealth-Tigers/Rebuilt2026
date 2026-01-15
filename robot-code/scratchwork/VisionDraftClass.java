593dd8eafe458b47d4167fa2520091b729f80e75
Rhys-Marriott <156854616+Rhys-Marriott@users.noreply.github.com>
2026-01-15 16:17:26 -0500

    Update VisionDraftClass.java to make methods public

--- robot-code/scratchwork/VisionDraftClass.java
+++ robot-code/scratchwork/VisionDraftClass.java
@@ -1,14 +1,14 @@
 public class VisionDraftClass {
-  static double visionx() {
+  public static double visionx() {
     return 2; 
   } // determine x coordinate of the robot, where x axis runs along long axis of field, in meters
-  static double visiony() {
+  public static double visiony() {
     return 4.034663;
   } // determine y coordinate of the robot, where y axis runs along short axis of field, in meters
-  static double visionz() {
+  public static double visionz() {
     return 0;
   } // determine z coordinate of the robot, where z axis is vertical, in meters
-  static double visionAngle() {
+  public static double visionAngle() {
     return 0;
   } // determine angle that the robot is facing where 0 is facing away from the driver station, in degrees
 }
