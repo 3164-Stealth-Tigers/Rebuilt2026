357ba349767cb693f07bd65f2332c7104b1112fc
Retroid <bububooo3@gmail.com>
2026-01-18 16:51:42 -0500

    edited superstructure

--- src/main/java/frc/robot/Superstructure.java
+++ src/main/java/frc/robot/Superstructure.java
@@ -7,10 +7,12 @@ import frc.robot.subsystems.vision.Vision;
 public class Superstructure {
     private final SwerveDrive swerve;
     private final Shooter shooter;
+    private final Vision vision;
 
     public Superstructure(SwerveDrive swerve, Vision vision, Shooter shooter) {
         this.swerve = swerve;
         this.shooter = shooter;
+        this.vision = vision;
     }
 
     public SwerveDrive getSwerve() {
@@ -19,6 +21,10 @@ public class Superstructure {
 
     public Shooter getShooter() {
         return shooter;
+    }
+
+    public Vision getVision() {
+        return vision;
     }
 
 }
